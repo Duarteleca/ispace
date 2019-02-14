7a1afa152e86840d8d9b04eb81075aaeaf171b0d
Marco Sá <marco2010_2011@hotmail.com>
2019-02-14 17:41:20 +0000

    asdasd

--- application/views/teste.php
+++ application/views/teste.php
@@ -1,18 +1,1 @@
-fewfewfewefwef
-vdvdfvdfvdfvdf
-wefwefwefwfe
-<?php
-
-<<<<<<< HEAD
-echo 'teste2 marco'; fwfewfewefwfewea
-asdasdasdasd
-
-=======
-echo 'teste2 marco'; fwfewfewefwfewe
-sdfasdfasdfasfd
-sdfasdfasdfasfdsdfsdf
-sdf
->>>>>>> marco
-?>
-
-
+5555
