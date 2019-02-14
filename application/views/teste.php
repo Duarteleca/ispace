155f5d4440244789951292d6e95bf1050ea06e0d
duarte leca <duarteleca@hotmail.com>
2019-02-14 17:48:45 +0000

    alteração

--- application/views/teste.php
+++ application/views/teste.php
@@ -3,16 +3,10 @@ vdvdfvdfvdfvdf
 wefwefwefwfe
 <?php
 
-<<<<<<< HEAD
-echo 'teste2 marco'; fwfewfewefwfewea
-asdasdasdasd
-
-=======
 echo 'teste2 marco'; fwfewfewefwfewe
 sdfasdfasdfasfd
 sdfasdfasdfasfdsdfsdf
 sdf
->>>>>>> marco
 ?>
 
 
