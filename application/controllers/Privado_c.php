cb6a7789114dc1e63fd1b52214e86f519b71fef2
Duarteleca <45486704+Duarteleca@users.noreply.github.com>
2019-02-15 13:08:17 +0000

    Update Privado_c.php

--- application/controllers/Privado_c.php
+++ application/controllers/Privado_c.php
@@ -18,6 +18,13 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+	public function __construct()
+	{
+			parent::__construct();
+			// $this->load->model('news_model');
+			$this->load->helper('url_helper');
+			
+	}
 	public function index()
 	{
 		$this->load->view('templates/header');
