497639599cb5e7a3065fe7f91c2df58945a2be60
Marco Sá <marco2010_2011@hotmail.com>
2019-02-15 09:57:46 +0000

    alteracao

--- application/views/teste.php
+++ application/views/teste.php
@@ -1,14 +1,2 @@
-<<<<<<< HEAD
-fewfewfewefwef
-vdvdfvdfvdfvdf
-wefwefwefwfe
-<?php
-
-echo 'teste2 marco'; fwfewfewefwfewe
-´2222
-?>
-
-
-=======
 5555
->>>>>>> ee757d568f9ce818fa84777dabd3a5cd827ce3f6
+ 
